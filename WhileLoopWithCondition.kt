3b99a5505090f10cc6a58ecb43eba601d10afeee
Bryan C <7554866+bryancandi@users.noreply.github.com>
2025-03-27 09:21:02 -0700

    Update WhileLoopWithCondition.kt
    Fixed formatting issue.

--- WhileLoopWithCondition.kt
+++ WhileLoopWithCondition.kt
@@ -1,14 +1,12 @@
 fun main() {
     var i = 0
-    while (i < 10)
-    if (i == 5)
-    {
-        i++
-        continue
-    }
-    else
-    {
-        println(i)
-        i++
+    while (i < 10) {
+        if (i == 5) {
+            i++
+            continue
+        } else {
+            println(i)
+            i++
+        }
     }
 }
